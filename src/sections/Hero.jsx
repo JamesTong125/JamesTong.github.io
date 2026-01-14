01ce3170a65f3722ad78803616f9a41d7ce4f77f
James Tong <james77.tong@gmail.com>
2026-01-14 12:26:46 -0500

    updated some stuff

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -69,7 +69,7 @@ const Hero = () => {
             <Button
               className="md:w-80 md:h-16 w-60 h-12"
               id="button"
-              text="Click Me!"
+              text="See my Work"
             />
 
             
